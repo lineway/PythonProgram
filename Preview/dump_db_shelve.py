6927d7783fcfaae5d1349fc2ba39a8b04b0a8f9e
Piels Zhang <zhangyiming07@163.com>
2016-12-01 00:31:30 +0800

    使用OOP实现之前功能

--- Preview/dump_db_shelve.py
+++ Preview/dump_db_shelve.py
@@ -1,6 +1,7 @@
 import shelve
 db = shelve.open('people-shevle')
 for key in db:
-	print(key, '=> \n', db[key])
-print(db['sue']['name'])
+	print(key, '=> \n ', db[key])
+# print(db['sue']['name'])
+print(db[u'sue'])
 db.close()
