bfcd1693b7dbc594f06b1f95575dc35d1ef67e0e
Piels Zhang <zhangyiming07@163.com>
2016-12-01 18:34:26 +0800

    add class shelve, and remove test database

---
+++ Preview/make_db_classes.py
@@ -0,0 +1,16 @@
+'''
+类的持久化，通过shelve存储媒介
+'''
+import shelve
+from person import Person
+from manager import Manager
+
+bob = Person('Bob Smith', 42, 30000, 'software')
+sue = Person('Sue Jones', 45, 40000, 'hardware')
+tom = Manager('Tom Doe', 50, 50000)
+
+db = shelve.open('class-shelve')
+db['bob'] = bob
+db['sue'] = sue
+db['tom'] = tom
+db.close()
--- Preview/manager.py
+++ Preview/manager.py
@@ -2,8 +2,7 @@ from person import Person
 
 class Manager(Person):
 	def giveRaise(self, percent, bonus=0.10):
-		self.pay *= (1.0 + percent + bonus)
-
+		Person.giveRaise(self, percent + bonus)
 
 if __name__ == '__main__':
 	tom = Manager(name='Tom Doe', age=50, pay=50000)
